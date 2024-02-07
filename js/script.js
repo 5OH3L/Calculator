a822b8f3c1dffd7e3990faad7c3f60e497250dfc
Sohel <152502004+5OH3L@users.noreply.github.com>
2024-02-07 22:06:53 +0530

    Made 4 functions for basic arithmetic.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let add = (num1, num2) =>{
+    return num1 + num2;
+};
+let substract = (num1, num2) =>{
+    return num1 - num2;
+};
+let multiply = (num1, num2) =>{
+    return num1 * num2;
+};
+let divide = (num1, num2) =>{
+    return num1 / num2;
+};
